881a0cf8700ee8938c044986d677111676239844
ramiksaier <ramos.ksaier@gmail.com>
2021-03-12 20:49:40 +0100

    second commit

--- Server.js
+++ Server.js
@@ -3,19 +3,23 @@ const app = express();
 const path = require("path");
 
 app.use(express.json());
-
+app.use((req, res, next) => {
+  const heur = timer.getHours;
+  const day = timer.getDay();
+  if (heur > 15 && heur < 17 && day >= 1 && day <= 5) {
+    res.next();
+  } else {
+    res.send(
+      `<img src="https://www.hostinger.fr/tutoriels/wp-content/uploads/sites/9/2017/05/comment-reparer-erreur-lors-de-la-connexion-a-la-bases-de-donnees-wordpress-1.png" alt="image1"/>
+      <br>
+      <h1>merci de connecter de lundi au vendredi de 7h jusqu'a 17:00</h1>
+      <h2>merci pour votre comprehension</h2>`
+    );
+  }
+});
 app.use(express.static(path.join(__dirname, "public")));
 const timer = new Date();
 console.log(timer.getHours());
-app.use((res, req, next) => {
-  const heur = timer.getHours;
-  const day = timer.getDay();
-  if (heur > 15  && heur < 17 || day >= 1 && day <= 5) {
-    res.next();
-  } else {
-    console.log("merci de connecter demain");
-  }
-});
 
 //create port
 const port = 5000;
